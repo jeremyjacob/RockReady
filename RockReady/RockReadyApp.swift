96cd98a76a79b1b1206bdb35d02399a18fc28ab4
jeremyjacob <github@jeremyjacob.dev>
2023-06-19 12:51:38 -0700

    Initial Commit

---
+++ RockReady/RockReadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockReadyApp.swift
+//  RockReady
+//
+//  Created by Jeremy Jacob on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct RockReadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
